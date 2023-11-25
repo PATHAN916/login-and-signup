06fb2e189375644bdbb159054a79dd19e6bc4516
pathan <rahamanpathan916@gmail.com>
2023-11-25 12:21:46 +0530

    add

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,49 @@
+import React, { useRef } from 'react'
+import axios from "axios"
+import { Link } from 'react-router-dom';
+
+function SignUp() {
+let nameInputRef=useRef();
+let emailInputRef=useRef();
+let passwordInputRef=useRef();
+
+let dataFromServer=async()=>{
+axios.defaults.baseURL="";
+let dataToSend=new FormData();
+dataToSend.append("name",nameInputRef.current.value);
+dataToSend.append("email",emailInputRef.current.value);
+dataToSend.append("password",passwordInputRef.current.value);
+
+let response= await axios.post("/signup",dataToSend);
+console.log(response);
+alert(response.data.msg)
+
+
+}
+  return (
+    <div className='App'>
+        <form>
+        <div>
+            <label>name</label>
+            <input ref={nameInputRef}></input>
+        </div>
+        <div>
+            <label>email</label>
+            <input ref={emailInputRef}></input>
+        </div>
+        <div>
+            <label>password</label>
+            <input ref={passwordInputRef}></input>
+        </div>
+        <div>
+            <button type='button' onClick={()=>{dataFromServer();}}>submit</button>
+        </div>
+
+        <Link to="/login">Login</Link>
+        </form>
+        
+    </div>
+  )
+}
+
+export default SignUp
